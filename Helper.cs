9d5528c51468fd762807f9e1905f2c1b59d3ee6b
logonoff <logonoff@users.noreply.github.com>
2022-06-10 15:10:48 -0400

    inital commit

---
+++ Helper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Win32;
+
+// <summary>
+// A static class with a bunch of helpers
+// </summary>
+public static class Helper
+{
+    // <summary>
+    // Returns a UNC (samba) path if it is a mapped drive, otherwise it returns the path unchanged
+    // See https://stackoverflow.com/a/28540229
+    // </summary>
+    public static string UNCPath(string path)
+    {
+        if (!path.StartsWith(@"\\"))
+        {
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey("Network\\" + path[0]))
+            {
+                if (key != null)
+                {
+                    return key.GetValue("RemotePath").ToString() + path.Remove(0, 2).ToString();
+                }
+            }
+        }
+        return path;
+    }
+}
---
+++ NeighbourhoodSnek.cs
@@ -0,0 +1,35 @@
+﻿using Nadeko.Snake;
+using NadekoBot.Services;
+
+public sealed class NeighbourhoodSnek : Snek
+{
+    private readonly CommandHandler _cmdHandler;
+
+    public NeighbourhoodSnek (
+        CommandHandler cmdHandler
+    )
+    {
+        _cmdHandler = cmdHandler;
+    }
+
+    /// <summary>
+    /// Queues local playlist while replacing mounted network path with direct UNC path
+    /// </summary>
+    [cmd]
+    public async Task NetworkPlay(GuildContext ctx, [leftover] string msg)
+    {
+        string message = Helper.UNCPath(msg);
+
+        await _cmdHandler.ExecuteExternal(ctx.Guild.Id, ctx.Channel.Id, ".localplaylist " + message);
+    }
+
+    /// <summary>
+    /// Hacky alias of NetworkPlay
+    /// </summary>
+    [cmd]
+    public async Task npl(GuildContext ctx, [leftover] string msg)
+    {
+        await NetworkPlay(ctx, msg);
+    }
+
+}
